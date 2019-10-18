b2a09a954ac25f08e671f9bef8fd7ea69f5dfc19
Black Blacksmith <andrx65@live.com>
2019-10-18 06:26:14 +0300

    1

--- GMapitest/Scripts/Home/gMap.js
+++ GMapitest/Scripts/Home/gMap.js
@@ -1,33 +1,29 @@
 ﻿let gMap = {
     mymap: {
         map: null,
-        bounds: null
+        bounds: null,
     },
     options:
     {
         googleKey: '',
         tagIg: 'map'
-
     },
 
-    init: function (items, options) {
+    init: function (items, options, findUser = true) {
         if (gMap.runOnce) {
             return;
         }
-        else {
+        else
+        {
             gMap.runOnce = true;
             gMap.options = $.extend(gMap.options, options);
             gMap.mymap.map = new google.maps.Map(document.getElementById('map'));
             gMap.mymap.bounds = new google.maps.LatLngBounds(); //объявление границ карты
+            //gMap.getLocation();
         }
     },
 
-    renderMap: function (locations, showRegions = true) {
-        //установка начала координат, если не определится местоположение 
-        let mapCenter;
-        if (!gMap._isPoint(mapCenter)) {
-            mapCenter = gMap._getCenter(locations);
-        }
+    renderMap: function (locations, showRegions = true, findUser = true) {
         //берем настройки из params 
         //визуализация в контейнере
         $(function initMap() {
@@ -54,21 +50,84 @@
                     //alert('error'); //записать в лог
                     continue;
                 }
+                gMap.mymap.map.fitBounds(gMap.mymap.bounds);
+                //gMap.getLocation();
+                
             }
-            gMap.mymap.map.fitBounds(gMap.mymap.bounds);
+            //gMap.showUser();
+            //gMap.getLocation();
         });
-        //let _newlocation = { lat: 43.6060604, lng: 39.7322588 }; // { lat: 43.6210114, lng: 39.7183147}; //тестовая точка для проверки доступности
-        //if (showRegions) gMap.showDestination(locations, _newlocation, true);
     },
 
-    //function that gets the location and returns it
-    getLocation: function () {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(
+    //получает местонахождение пользователя, вызов коллбэка при успехе/неудаче
+    getLocation: function ()
+    {
+        if (navigator.geolocation)
+        {
+            navigator.geolocation.getCurrentPosition( 
                 gMap._geolocationSuccess, gMap._geolocationFailure);
         } else {
             console.log("Геолокация не поддерживается вашим устройством");
         }
+    },
+
+    _geolocationSuccess: function (position) {
+        // Преобразуем местоположение в объект LatLng
+        let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        // Отображаем эту точку на карте
+        if (position.coords.accuracy < 5000) {
+            gMap.showUser(location);
+            gMap.mymap.bounds.extend(location);
+        }
+        else {
+            console.log(position.coords.accuracy);
+            alert('Координаты определены с погрешностью.');
+        }
+    },
+
+    _geolocationFailure: function (positionError) {
+        console.log(positionError);
+        alert('Позиция не определена');
+    },
+
+   
+    //отображаем локацию пользователя на карте в точке с переданными координатами
+    _setUser: function (location)
+    {
+
+    },
+
+    showUser: function (location)
+    {
+ 
+        let image = {
+            url: '/LocLogo.png',
+            size: new google.maps.Size(40, 40),
+            scaledSize: new google.maps.Size(40, 40)
+        };
+        let marker = new google.maps.Marker(
+            {
+                position: location,
+                map: gMap.mymap.map,
+                title: 'Вы здесь',
+                icon: image,
+            });
+        //alert('marker');
+        gMap.mymap.bounds.extend(location);
+    },
+
+    //проверка корректности позиции
+    _isPoint: function (location) {
+        if (location === undefined
+            || location === null
+            || isNaN(location.lat)
+            || isNaN(location.lng)
+            || location.lat < -90
+            || location.lat > 90
+            || location.lng < -180
+            || location.lng > 180)
+            return false;
+        else return true;
     },
 
     showDestination: function (locations, userLocation, showAll = false) {
@@ -89,58 +148,6 @@
         return isInside;
     },
 
-    _setUser: function (location) {
-        console.log(location);
-        let image = {
-            url: '/LocLogo.png',
-            size: new google.maps.Size(40, 40),
-            scaledSize: new google.maps.Size(40, 40)
-        };
-        let marker = new google.maps.Marker(
-            {
-                position: location,
-                map: gMap.mymap.map,
-                title: 'Вы здесь',
-                icon: image,
-            });
-        //alert('marker');
-        gMap.mymap.bounds.extend(location);
-    },
-
-    //внутренние методы
-    //проверка корректности позиции
-    _isPoint: function (location) {
-        if (location === undefined
-            || location === null
-            || isNaN(location.lat)
-            || isNaN(location.lng)
-            || location.lat < -90
-            || location.lat > 90
-            || location.lng < -180
-            || location.lng > 180)
-            return false;
-        else return true;
-    },
-
-    _geolocationSuccess: function (position) {
-        // Преобразуем местоположение в объект LatLng
-        let location = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-        // Отображаем эту точку на карте
-        if (position.coords.accuracy < 5000) {
-            gMap._setUser(location);
-            gMap.mymap.bounds.extend(location);
-        }
-        else {
-            console.log(position.coords.accuracy);
-            alert('Координаты определены с погрешностью.');
-        }
-    },
-
-    _geolocationFailure: function (positionError) {
-        console.log(positionError);
-        alert('Позиция не определена');
-    },
-
     _getCenter: function (locations) {
         let lat = 0;
         let lng = 0;
@@ -154,6 +161,9 @@
         let centerLocation = { lat: lat, lng: lng };
         return centerLocation;
     },
+
+    findLocationInRange: function ()
+    {
+
+    },
 };
-
-
